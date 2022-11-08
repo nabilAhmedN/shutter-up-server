86cb39c9f2b67c86211f034ea2bcb08d6808080f
nabilAhmedN <nabilahmednahid@gmail.com>
2022-11-08 23:12:29 +0600

    dynamic services added

--- index.js
+++ index.js
@@ -40,7 +40,12 @@ async function run() {
             res.send(server);
         });
 
-        
+        app.get("/services/:id", async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        })
     } 
     finally {
 
